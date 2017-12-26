601139efe190651f3eff2bb366c13f4f2f6f988d
wangqiang <wangqiang@intra.customs.gov.cn>
2017-12-26 15:03:57 +0800

    改造springboot项目,支持weblogic部署

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,17 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 /**
  * File Header
  * PROJECT_NAME: ws-demo
  * PACKAGE_NAME: com.example.demo
  *
- * @author Kyou
- * @date 2017/12/23 16:57
+ * @author Kyou 2017/12/23 16:57
  */
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
